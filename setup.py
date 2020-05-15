88cea55d22d4126724932bbe9d2640adfb77a198
MJ-Jang <xkxpa@korea.ac.kr>
2020-05-15 13:53:09 +0900

    update setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setup(
+    name='orca',
+    version='0.1.1',
+    description='Korean typo correcter',
+    author='MJ Jang',
+    install_requires=[],
+    packages=find_packages(exclude=['docs', 'tests', 'tmp', 'data']),
+    python_requires='>=3',
+    zip_safe=False,
+    include_package_data=True
+)
