3c009b53e5aee6b05a6eda4f75e0852ef6a9cc5f
MJ-Jang <xkxpa@korea.ac.kr>
2020-06-01 11:47:35 +0900

    update typo generation code

--- orca/dataset/dataset.py
+++ orca/dataset/dataset.py
@@ -44,8 +44,12 @@ class TypoDetectionSentenceLevelDataset(Dataset):
 
         for word in words:
             y = 0
-            if random.random() <= 0.5:
-                _, word = noise_maker(word, 1.0, self.typo_nu)
+            prob_ = random.random()
+            if prob_ <= 0.5:
+                if prob_ <= 0.3:
+                    _, word = noise_maker(word, 1.0, 1, method='ct')
+                else:
+                    _, word = noise_maker(word, 1.0, self.typo_nu, method='g')
                 y = 1
             token = self.tokenizer.text_to_idx(word)
             if len(token) <= self.max_word_len:
@@ -60,130 +64,5 @@ class TypoDetectionSentenceLevelDataset(Dataset):
         else:
             inputs = inputs[:self.max_sent_len]
             targets = targets[:self.max_sent_len]
-
-        return np.array(inputs), np.array(targets)
-
-
-class TypoDetectionDataset(Dataset):
-
-    def __init__(self,
-                 sents: list,
-                 typo_num: int = 2,
-                 max_len: int = 20
-                 ) -> None:
-
-        self.tokenizer = CharacterTokenizer()
-        self.vocab_size = len(self.tokenizer)
-        words = set()
-
-        for s in tqdm(sents, desc='splitting words'):
-            for w in s.split(' '):
-                if self.is_ko_word(w):
-                    words.add(w)
-
-        self.words = list(words)
-        self.typo_nu = typo_num
-        self.max_len = max_len
-        self.pad_id = self.tokenizer.pad_id
-
-    def __len__(self):
-        return len(self.words)
-
-    def __getitem__(self, item):
-        word = self.words[item]
-        y = 0
-        if random.random() <= 0.4:
-            _, word = noise_maker(word, 1.0, self.typo_nu)
-            y = 1
-        token = self.tokenizer.text_to_idx(word)
-
-        if len(token) <= self.max_len:
-            token += [self.pad_id] * (self.max_len - len(token))
-        else:
-            token = token[:self.max_len]
-        return np.array(token), y
-
-    @staticmethod
-    def is_ko_word(word: str):
-        match_number = re.match(pattern='\\d+', string=word)
-        match_eng = re.match(pattern='[a-zA-Z]+', string=word)
-        if len(word) <= 1:
-            return False
-        if match_eng:
-            s, e = match_eng.span()
-            if word == word[s:e]:
-                return False
-        if match_number:
-            s, e = match_number.span()
-            if word == word[s:e]:
-                return False
-        return True
-
-
-class CBOWTypoDataset(Dataset):
-
-    def __init__(self,
-                 sents: list,
-                 window_size: int
-                 ) -> None:
-        assert window_size > 0
-
-        self.tokenizer = CharacterTokenizer()
-        self.vocab_size = len(self.tokenizer)
-        self.data = []
-
-        for s in tqdm(sents):
-            token = self.tokenizer.text_to_idx(s)
-            token = [self.tokenizer.pad_id] * window_size + token + [self.tokenizer.pad_id] * window_size
-            for i in range(window_size, len(token) - window_size):
-                context = [token[i - 2], token[i - 1],
-                           token[i + 1], token[i + 2]]
-                target = token[i]
-                self.data.append((context, target))
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        inp, tgt = self.data[idx][0], self.data[idx][1]
-        return np.array(inp), tgt
-
-
-class TextCNNDataset(Dataset):
-
-    def __init__(self,
-                 sents: list,
-                 max_len: int = 150,
-                 threshold: float = 0.3,
-                 noise_char_ratio: float = 0.05):
-
-        self.tokenizer = CharacterTokenizer()
-        self.vocab_size = len(self.tokenizer)
-        self.data = sents
-
-        self.max_len = max_len
-        self.threshold = threshold
-        self.noise_char_ratio = noise_char_ratio
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, item):
-        sent = self.data[item]
-
-        targets = self.tokenizer.text_to_idx(sent)
-        inputs, _ = noise_maker(sent, self.threshold, self.noise_char_ratio)
-        tok_to_id_dict = self.tokenizer.tok_to_id_dict
-        inputs = [tok_to_id_dict[v] if v in list(tok_to_id_dict.keys()) else self.tokenizer.unk_id for v in inputs]
-
-        assert len(targets) == len(inputs)
-
-        if len(targets) <= self.max_len:
-            targets += [self.tokenizer.pad_id] * (self.max_len - len(targets))
-            inputs += [self.tokenizer.pad_id] * (self.max_len - len(inputs))
-        else:
-            print(self.max_len)
-            targets = targets[:self.max_len]
-            inputs = inputs[:self.max_len]
 
         return np.array(inputs), np.array(targets)
--- orca/utils/noise_functions.py
+++ orca/utils/noise_functions.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals, print_function, division
 
 from orca.tokenizer.pattern import *
 from orca.utils.hangeul import flat_hangeul, merge_flatted_hangeul
+from orca.utils.pattern import typo_pattern
 
 import random
 
@@ -17,19 +18,33 @@ for pattern in [KOR_CHAR_JA_BOTH_PATTERN, KOR_CHAR_JA_UNDER_PATTERN, KOR_CHAR_MO
         noise_dict[s] = pattern[:i] + pattern[i+1:]
 
 
-def noise_maker(text: str, threshold: float = 0.3, noise_char_num: int = 2):
+def noise_maker(word: str, threshold: float = 0.3, noise_char_num: int = 2, method: str = 'g'):
+    """
+    :param word: single word
+    :param threshold: probability of generating noise
+    :param noise_char_num: maximum number of noise character
+    :param method: generation method, ("g": general, "ct": close typo)
+    """
+    noise_dict_key = {
+        'g': noise_dict,
+        'ct': typo_pattern
+    }
+    if method not in ['g', 'ct']:
+        raise ValueError("specified generation method is not supported")
+    n_dict = noise_dict_key[method]
+
     assert threshold >= 0
     assert threshold <= 1
 
-    text_tokenized = flat_hangeul(text)
+    text_tokenized = flat_hangeul(word)
 
     rand = random.random()
     if rand >= threshold:
-        return text_tokenized, text
+        return text_tokenized, word
     else:
         sample_num_noise = random.choice(range(1, noise_char_num+1))
         rand_idx = random.sample(range(len(text_tokenized)), min(len(text_tokenized), sample_num_noise))
         for idx in rand_idx:
-            if text_tokenized[idx] in list(noise_dict.keys()):
-                text_tokenized[idx] = random.choice(noise_dict[text_tokenized[idx]])
+            if text_tokenized[idx] in list(n_dict.keys()):
+                text_tokenized[idx] = random.choice(n_dict[text_tokenized[idx]])
         return text_tokenized, merge_flatted_hangeul(text_tokenized)
