b504a42d826302971b6a284f10f84600607063d9
MJ-Jang <xkxpa@korea.ac.kr>
2020-06-02 18:35:35 +0900

    update data and examples

--- example/train_correction.py
+++
@@ -1,7 +0,0 @@
-from orca.correction import SymDeletingTypoCorrecter
-
-aa = SymDeletingTypoCorrecter()
-aa.train('../data/train_data.txt', 'data/', 'test_uni', 'test_bi')
-aa.load_model('./data/test_uni.txt', './data/test_bi.txt')
-print(aa.infer('인청'))
-
