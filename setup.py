a62bd6d427692b0ab992a864c219f7856ca17d65
MJ-Jang <xkxpa@korea.ac.kr>
2020-06-03 14:28:53 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version='0.1.3',
     description='Korean typo correcter',
     author='MJ Jang',
-    install_requires=[],
+    install_requires=required,
     packages=find_packages(exclude=['docs', 'tests', 'tmp', 'data', '__pycache__']),
     python_requires='>=3',
     package_data={'orca': ['resources/*']},
